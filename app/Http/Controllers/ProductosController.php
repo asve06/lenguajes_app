e28f68d64608f0c8ab35aa85825dd743817929ce
leolopezv <leonardo.lopez@uees.edu.ec>
2024-02-10 17:16:47 -0500

    Models y Controllers

---
+++ app/Http/Controllers/ProductosController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+use Illuminate\Support\Facades\DB;
+use App\Models\Productos;
+use Illuminate\Http\Request;
+
+class ProductosController extends Controller
+{
+    
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Productos $productos)
+    {
+        //
+    }
+
+    public function edit(Productos $productos)
+    {
+        //
+    }
+
+    public function update(Request $request, Productos $productos)
+    {
+        //
+    }
+
+    public function destroy(Productos $productos)
+    {
+        //
+    }
+}
