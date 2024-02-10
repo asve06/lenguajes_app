574a5acc2cf2079abca4e3fa4b1890f5f0d19818
leolopezv <leonardo.lopez@uees.edu.ec>
2024-02-10 16:48:35 -0500

    Codigo de base de datos final

---
+++ database/migrations/2024_02_10_000004_create_ingresos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngresosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingresos', function (Blueprint $table) {
+            $table->id('ingresoID');
+            $table->integer('cantidad ingresada');
+            $table->timestamps();
+            $table->foreignId('productoId')->constrained('productos', 'productoID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingresos');
+    }
+}
---
+++ database/migrations/2024_02_10_000005_create_egresos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEgresosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('egresos', function (Blueprint $table) {
+            $table->id('egresoID');
+            $table->integer('cantidad egresada');
+            $table->timestamps();
+            $table->foreignId('productoId')->constrained('productos', 'productoID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('egresos');
+    }
+}
